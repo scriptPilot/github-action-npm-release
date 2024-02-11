e85fa2c14c139703a33d8be300d97698c08f1b8d
scriptPilot <dennis@scheffel.de>
2024-02-11 23:39:06 +0100

    initial release
    closes #1

---
+++ README.md
@@ -0,0 +1,36 @@
+# NPM Release GitHub Action
+
+This GitHub workflow actions simplifies the release of new versions.
+
+## Scope
+
+- ✅ NPM release
+- ✅ GitHub release 
+- ✅ GitHub release change note
+
+## Usage
+
+1. Create an NPM access token for automatisation.
+2. Add this token as action secret to the GitHub repository.
+3. Create a workflow file `.github/workflows/release.yml`:
+
+```yml
+on:
+  push:
+    branches:
+      - main
+jobs:
+  release:
+    runs-on: ubuntu-latest
+    permissions: write-all    
+    steps:        
+      - uses: actions/checkout@v4
+      - run: npm ci
+      - run: scriptpilot/github-action-npm-release
+        env:
+          GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
+          NPM_TOKEN: ${{ secrets.NPM_TOKEN }}
+```
+
+4. Increase the version in the `package.json` file.
+5. Commit and push the change to the repository.
---
+++ index.js
@@ -0,0 +1,43 @@
+const shell = require('shelljs')
+const fs = require('fs-extra')
+const fetch = require('node-fetch')
+
+// Read the package version
+const packageJson = fs.readJsonSync('./package.json')
+console.log(`Version in package.json file: ${packageJson.version}`)
+
+// Read all NPM versions
+const npmResp = await fetch(`https://registry.npmjs.org/${packageJson.name}`)
+const npmJson = await npmResp.json()
+const npmVersions = Object.keys(npmJson.versions)
+console.log(`Versions in NPM repository:`, npmVersions)
+
+// Exit the script if the package version is already published on NPM
+if (npmVersions.includes(packageJson.version)) {
+  console.log(`Version ${packageJson.version} already published to NPM.`)
+  process.exit(0)
+}
+
+// Run the test script
+if (shell.exec('npm run test').code !== 0) process.exit(1)
+
+// Run the build script
+if (shell.exec('npm run build').code !== 0) process.exit(1)
+
+// Publish to NPM
+if (process.env.NPM_TOKEN) {
+  fs.writeFileSync('.npmrc', `//registry.npmjs.org/:_authToken=${process.env.NPM_TOKEN}`)
+  if (shell.exec('npm publish --access public').code !== 0) process.exit(1)
+}
+
+// Create a Git tag
+if (shell.exec(`git tag "v${packageJson.version}"`).code !== 0) process.exit(1)
+if (shell.exec(`git push origin "v${packageJson.version}"`).code !== 0) process.exit(1)
+
+// Create a GitHub release
+if (shell.exec(`gh release create "v${packageJson.version}"`).code !== 0) process.exit(1)
+
+// Update the GitHub release notes
+if (shell.exec(`cd ./node_modules/github-release-notes && npm link`).code !== 0) process.exit(1)
+if (shell.exec(`gren release --tags "v${packageJson.version}" \
+                --token "${process.env.GITHUB_TOKEN}" --override`).code !== 0) process.exit(1)
